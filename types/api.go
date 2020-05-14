f2633a61a4fc1fa8a2f8846c5a495277f620ef73
p4u <pau@dabax.net>
2020-05-14 18:57:44 +0200

    add some API structs
    Signed-off-by: p4u <pau@dabax.net>

--- types/api.go
+++ types/api.go
@@ -1,6 +1,8 @@
 package types
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type RequestMessage struct {
 	MetaRequest `json:"request"`
@@ -10,10 +12,17 @@ type RequestMessage struct {
 }
 
 type MetaRequest struct {
-	EntityId  string `json:"entityId,omitempty"`
-	Method    string `json:"method"`
-	Signature string `json:"signature,omitempty"`
-	Timestamp int32  `json:"timestamp"`
+	Census      *Census      `json:"census,omitempty"`
+	EntityID    string       `json:"entityId,omitempty"`
+	Filter      *Filter      `json:"filter,omitempty"`
+	ListOptions *ListOptions `json:"listOptions,omitempty"`
+	Member      *Member      `json:"member"`
+	Method      string       `json:"method"`
+	Signature   string       `json:"signature,omitempty"`
+	Scope       string       `json:"scope,omitempty"`
+	Status      *Status      `json:"status,omitempty"`
+	Timestamp   int32        `json:"timestamp"`
+	Token       string       `json:"token,omitempty"`
 }
 
 // ResponseMessage wraps an api response
@@ -28,11 +37,12 @@ type ResponseMessage struct {
 // Fields must be in alphabetical order
 // Those fields with valid zero-values (such as bool) must be pointers
 type MetaResponse struct {
-	Message   string `json:"message,omitempty"`
-	Ok        bool   `json:"ok"`
-	PublicKey string `json:"publicKey"`
-	Request   string `json:"request"`
-	Timestamp int32  `json:"timestamp"`
+	Message   string  `json:"message,omitempty"`
+	Ok        bool    `json:"ok"`
+	PublicKey string  `json:"publicKey"`
+	Request   string  `json:"request"`
+	Status    *Status `json:"status,omitempty"`
+	Timestamp int32   `json:"timestamp"`
 }
 
 // SetError sets the MetaResponse's Ok field to false, and Message to a string
@@ -40,4 +50,38 @@ type MetaResponse struct {
 func (r *MetaResponse) SetError(v interface{}) {
 	r.Ok = false
 	r.Message = fmt.Sprintf("%s", v)
+}
+
+type Member struct {
+	DateOfBirth string `json:"dateOfBirth,omitempty"`
+	Email       string `json:"email,omitempty"`
+	FirstName   string `json:"firstName,omitempty"`
+	LastName    string `json:"lastName,omitempty"`
+	Phone       string `json:"phone,omitempty"`
+}
+
+type Status struct {
+	Registered  bool `json:"registered,omitempty"`
+	NeedsUpdate bool `json:"needsUpdate,omitempty"`
+}
+
+type Census struct {
+	Created int32  `json:"created,omitempty"`
+	ID      string `json:"id,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Root    string `json:"root,omitempty"`
+	Target  string `json:"target,omitempty"`
+	URI     string `json:"uri,omitempty"`
+}
+
+type ListOptions struct {
+	Count  int    `json:"count,omitempty"`
+	Order  string `json:"order,omitempty"`
+	Skip   int    `json:"skip,omitempty"`
+	SortBy string `json:"sortBy,omitempty"`
+}
+
+type Filter struct {
+	Member
+	Target string `json:"target,omitempty"`
 }
