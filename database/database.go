b712ecd60a47f15642ff53298c079e84af36e0ef
div dev doe <divdevdoe@riseup.net>
2020-05-26 13:14:47 +0000

    Implements First queries

--- database/database.go
+++ database/database.go
@@ -7,8 +7,10 @@ import (
 
 type Database interface {
 	Close() error
+	CreateEntity(entityID string, address string, email string, name string, censusManagersAddresses []string) (*types.Entity, error)
 	Entity(entityID string) (*types.Entity, error)
 	EntityHas(entityID string, memberID uuid.UUID) bool
+	CreateMember(entityID string) (*types.Member, error)
 	Member(memberID uuid.UUID) (*types.Member, error)
 	Census(censusID string) (*types.Census, error)
 }
---
+++ database/pgsql/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Creates a database, user, and extensions to use Devicehub
+# $DB is the database to create
+# $USER is the user to create and give full permissions on the database
+# This script asks for the password of such user
+set -e
+DB='vocdonimgr'
+
+# read -s -p "Password for $USER": pass
+# createdb $DB  # Create main database
+psql --username "$POSTGRES_USER" --dbname "postgres" --command "CREATE DATABASE $DB ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';"
+# psql -d $DB -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';" # Create user Devicehub uses to access db
+psql --username "$POSTGRES_USER" --dbname "$DB" -c "GRANT ALL PRIVILEGES ON DATABASE $DB TO $POSTGRES_USER;" # Give access to the db
+psql --username "$POSTGRES_USER" --dbname "$DB"  -c "CREATE EXTENSION pgcrypto SCHEMA public;" # Enable pgcrypto
+psql --username "$POSTGRES_USER" --dbname "$DB" -a -f /root/schema.psql
