526ad763e88519c9de5c728b56be3c836a1abdc9
divdevdoe <divdevdoe@riseup.net>
2020-06-16 19:58:09 +0200

    Adds extrautil functions to calculate ID and Address from signer

--- util/util.go
+++ util/util.go
@@ -13,9 +13,6 @@ func PubKeyToEntityID(pubKey string) ([]byte, error) {
 	var address []byte
 	var err error
 	if address, err = PubKeyToAddress(pubKey); err != nil {
-		// log.Warn(err)
-		// m.Router.SendError(request, "cannot decode public key")
-		// return
 		return nil, fmt.Errorf("cannot decode public key")
 	}
 	return ethereum.HashRaw(address), nil
@@ -32,4 +29,25 @@ func PubKeyToAddress(pubKey string) ([]byte, error) {
 		return nil, fmt.Errorf("Error extracting address from public key %w", err)
 	}
 	return addressB, nil
+}
+
+func SignerEntityAddress(singer ethereum.SignKeys) ([]byte, error) {
+	// retrieve entity ID
+	var address []byte
+	var err error
+	addressString := singer.EthAddrString()
+	if address, err = hex.DecodeString(addressString); err != nil {
+		return nil, fmt.Errorf("cannot decode entityAddress")
+	}
+	return address, nil
+}
+
+func SignerEntityID(singer ethereum.SignKeys) ([]byte, error) {
+	// retrieve entity ID
+	var address []byte
+	var err error
+	if address, err = SignerEntityAddress(singer); err != nil {
+		return nil, fmt.Errorf("cannot decode entityAddress")
+	}
+	return ethereum.HashRaw(address), nil
 }
