f663e9854d471573370c2c2ec8ffe900e7982633
emmdim <emmdim@users.noreply.github.com>
2021-05-26 11:16:52 +0200

    Removes goerli from tests

--- test/testethclient/ethclient_test.go
+++ test/testethclient/ethclient_test.go
@@ -18,7 +18,6 @@ var signer *ethereum.SignKeys
 
 var testNetworks = []config.EthNetwork{
 	{Name: "xdai", Provider: "https://xdai1.vocdoni.net", Timeout: 60},
-	{Name: "goerli", Provider: "https://goerli.vocdoni.net", Timeout: 60},
 }
 
 func TestMain(m *testing.M) {
