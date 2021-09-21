1efd321e16c8b74f4fa46ccaba31f207e9eec005
emmdim <emmdim@users.noreply.github.com>
2021-09-21 17:23:18 +0200

    Adds type and size type in the AdminEntityList metrics

--- database/pgsql/pgsql.go
+++ database/pgsql/pgsql.go
@@ -279,7 +279,7 @@ func (d *Database) AdminEntityList() ([]types.Entity, error) {
 	// 		MemberID: memberID.String(),
 	// 	}
 	// }
-	query := `SELECT name, encode(id,'hex') as ID, email, created_at, updated_at FROM entities  where created_at > '2021-05-18' AND LOWER(email) not like 
+	query := `SELECT name, encode(id,'hex') as ID, email, created_at, updated_at, type, size FROM entities  where created_at > '2021-05-18' AND LOWER(email) not like 
 	ANY(ARRAY[LOWER('%vocdoni%'),LOWER('%alexflores%'),LOWER('%aragon%'),LOWER('%test@test.com%'),
 			  LOWER('%pinyana%'),LOWER('%div@mail.com%'),LOWER('%mail@mail.com%'), LOWER('%test@example.com%'), LOWER('%joanarus%'), LOWER('%guifre.ballester%')]);`
 	if err := d.db.Select(&entities, query); err != nil {
